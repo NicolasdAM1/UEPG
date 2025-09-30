37fdfb386d1ab967a1cb346a4d7f7a729755f996
NicolasdAM1 <nicoantoni15@gmail.com>
2025-09-29 21:50:01 -0300

    Lista de Ponteiros II

---
+++ C/Lista de Ponteiros II/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+   //Ex.: 1
+     
+
+
+   /*Ex.: 4
+    int a, *b, **c, ***d;
+    printf("Digite um valor inteiro:\n> ");
+    scanf("%d",&a);
+    b = &a;
+    c = &b;
+    d = &c;
+    printf("Dobro: %d\nTriplo: %d\nQuádruplo: %d", 2 * (*b), 3 * (**c), 4 * (***d));*/
+
+    return 0;
+}

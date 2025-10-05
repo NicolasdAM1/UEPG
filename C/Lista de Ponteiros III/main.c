8f918b67a6d19a1a9863fa3d95f129df6a911531
NicolasdAM1 <nicoantoni15@gmail.com>
2025-10-05 13:30:31 -0300

    Finalizando a lista de ponteiros III

--- C/Lista de Ponteiros III/main.c
+++ C/Lista de Ponteiros III/main.c
@@ -6,8 +6,24 @@
 
 int main(){
 
+   /*Ex.: 1
+    float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0 };
+    float *p = array;
+    for(int i = 0; i <= 9; i++){
+        printf("%p\n",p + i);
+    } */
 
-    
+   /*Ex.: 2
+    float matriz[3][3] = { {1.1, 2.2, 3.3}, {4.4, 5.55, 6.6}, {7.7, 8.8, 9.9} };
+    float *p = &matriz[0][0];
+    for(int i = 0; i <= 8; i++){
+        printf("%p\n", p + i);
+    } */
+
+   //Ex.: 3
+     
+
+
 
     return 0;
 }

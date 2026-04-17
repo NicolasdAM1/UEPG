4ef6b1e74d21b0b31d1f62fccd218b932c99fc0d
NicolasdAM1 <nicoantoni15@gmail.com>
2026-04-17 20:13:19 -0300

    aula 14 e aula 15 de java

---
+++ Java/Aula15/src/Fatorial.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import aula15.*;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Fatorial {
+    
+    private int N = 0;
+    private int F = 1;
+    private String FORM = "";
+    
+    public void setValor(int n){
+        N = n;
+        int f = 1;
+        String s = "";
+        for(int i = n; i > 1; i--){
+            f *= i;
+            s += i + " x ";
+        }
+        s += "1 = ";
+        F = f;
+        FORM = s;
+    }
+    
+    public int getFatorial(){
+        return F;
+    }
+    
+    public String getFormula(){
+        return FORM;
+    }
+}

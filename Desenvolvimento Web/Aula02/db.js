57dd9942ab45a934328e236d1c3b5a2d181f2735
NicolasdAM1 <nicoantoni15@gmail.com>
2026-07-27 21:41:08 -0300

    Agenda

---
+++ Desenvolvimento Web/Aula02/db.js
@@ -0,0 +1,18 @@
+const agenda = [{
+    id: 1,
+    nome: "Nome 01",
+    telefone: 42900000001,
+    email: "example01@mail.com",
+    nota: "Primeiro",
+    ativo: true
+}, {
+    id: 2,
+    nome: "Nome 02",
+    telefone: 42900000002,
+    email: "example02@mail.com",
+    nota: "É o Segundo",
+    ativo: true
+}
+];
+
+export default agenda;

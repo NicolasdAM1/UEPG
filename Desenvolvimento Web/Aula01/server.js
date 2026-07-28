e66ca652c1db8671a4d2df9dd335e2f1f5958580
NicolasdAM1 <nicoantoni15@gmail.com>
2026-07-27 21:32:24 -0300

    Teste

---
+++ Desenvolvimento Web/Aula01/server.js
@@ -0,0 +1,18 @@
+// Aula 01
+
+import http from 'http';
+
+function processa(req, res){
+    if (req.method === 'GET' && req.url === '/teste'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Funcionou</h1>');
+        res.end();
+    }
+    else {
+        res.writeHead(404);
+        res.end('Não encontrado');
+  }
+}
+const servidor = http.createServer(processa);
+servidor.listen(3000);
+console.log('Servidor ON');
